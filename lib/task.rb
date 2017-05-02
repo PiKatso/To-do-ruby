e5e11988ace01f1ed38cfc000e15f4a66714252b
Ben Williams and Sara Schultz <sara@>
2017-05-02 09:29:06 -0700

    pass spec for == method

--- lib/task.rb
+++ lib/task.rb
@@ -3,7 +3,9 @@ class Task
   define_method(:initialize) do |description|
     @description = description
   end
-
+  define_method(:==) do |another_task|
+    self.description.==(another_task.description)
+  end
   define_method(:description) do
     @description
   end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,9 +1,22 @@
 require('rspec')
+require 'pg'
 require('task')
 
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
+
 describe(Task) do
-  before() do
-    Task.clear()
+  describe '#==' do
+    it 'is the same task if it has the same description' do
+      task1 = Task.new({:description => 'learn SQL'})
+      task2 = Task.new({:description => 'learn SQL'})
+      expect(task1).to(eq(task2))
+    end
   end
   describe("#description") do
     it("lets you give it a description") do
